1ef419569a2e8ac41232f323dedbd5b76b7a9e12
Nursultan721 <avazalievnursultan13@gmail.com>
2025-10-23 18:35:48 +0600

    change

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ export const Header = ({setOpen}) => {
     return (
         <header>
             <nav className='links'>
-                <NavLink to={'/'}><li>Главная</li></NavLink>
+                <NavLink to={'/'}><li>Главнаая</li></NavLink>
                 <NavLink to={'/menu-page'}><li>Меню</li></NavLink>
                 <NavLink to={'/basket-page'}><li>Корзина <sub>{length}</sub> </li></NavLink>
                 <button onClick={() => {setOpen(true)}}>Бронь столика</button>
